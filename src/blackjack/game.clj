f11a09d080da49b4730ea66176e0f8e5a4a05aa8
fimmtiu <dennis@funkplanet.com>
2013-07-30 19:34:32 -0700

    Stubbed-out version of Game.

---
+++ src/blackjack/game.clj
@@ -0,0 +1,23 @@
+(ns blackjack.game
+  (:use clojure.math.combinatorics))
+
+(defrecord Game [deck players])
+
+;; REPL demo, next 3 expressions
+;; Do (use 'clojure.math.combinatorics) first
+
+(def card-suits [:spades :hearts :diamonds :clubs])
+(def card-values (range 1 14))
+
+(defn- make-deck []
+  (shuffle (cartesian-product card-suits card-values)))
+
+(defn make-game []
+  (->Game (make-deck) []))   ;; we'll add players shortly
+
+(defn play-round [game]
+  )
+
+(defn winner [game]
+  nil)
+

793fa0468601fc6ead671e11540c2459d66f4dd9
fimmtiu <dennis@funkplanet.com>
2013-07-30 19:33:41 -0700

    Fixed project.clj indentation.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject blackjack "0.1"
   :description "A didactic blackjack-playing program"
   :url "http://github.com/fimmtiu/lunch-lecture"
-  :dependencies [[org.clojure/clojure "1.4.0"] [org.clojure/math.combinatorics "0.0.4"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/math.combinatorics "0.0.4"]]
   :main blackjack.core)

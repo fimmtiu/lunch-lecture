dbb2d76f9912378b62fb8548922bdd419524dc6f
fimmtiu <dennis@funkplanet.com>
2013-08-04 21:53:32 -0700

    Merged core and game.

--- src/blackjack/core.clj
+++
@@ -1,9 +0,0 @@
-(ns blackjack.core
-  (:use blackjack.game)
-  (:gen-class))
-
-(defn -main [& args]
-  (let [game (make-game)]
-    (while (not (winner game))
-      (play-round game))
-    (println "Game over!" (winner game) "wins.")))
--- src/blackjack/game.clj
+++ src/blackjack/game.clj
@@ -1,24 +1,13 @@
 (ns blackjack.game
-  (:use clojure.math.combinatorics))
+  (:use blackjack.deck)
+  (:gen-class))
 
-(defrecord Game [deck players])
-
-;; REPL demo, next 3 expressions
-;; Do (use 'clojure.math.combinatorics) first
-
-(def card-suits [:spades :hearts :diamonds :clubs])
-(def card-values (range 1 14))
-
-(defn- make-deck []
-  (shuffle (cartesian-product card-suits card-values)))
-
-(defn make-game []
-  (->Game (make-deck) []))   ;; we'll add players shortly
-
-(defn play-round [game]
-  )
-
-;; Change this to "Balls" or something to make sure it works.
-(defn winner [game]
+(defn- play-round []
   nil)
 
+(defn- keep-going? []
+  nil)
+
+(defn -main [& args]
+  (while (keep-going?)
+    (play-round)))
